2320fb35931cb6529771ea00d7cafce910fc4018
gabrielaferreiraa <gabxfo@gmail.com>
2025-04-21 12:47:28 +0000

    Correções segundo desafio

--- Super trunfo 2.c
+++ Super trunfo 2.c
@@ -38,6 +38,7 @@ int main() {
     float PIB[2];
     int numeroPt[2];
     float densidadeInvertida[2];
+    int escolha;
 
     // Cadastro das Cartas:
     printf ("Atributos das cidades\n Digite o código da cidade 1:");
@@ -69,104 +70,114 @@ int main() {
     scanf ("%d", &numeroPt[0]);
     printf ("Digite o número dos pontos turísticos da cidade 2:");
     scanf ("%d", &numeroPt[1]);
-    
+
     Cidade cidade[2] = {
         {populacao[0], area[0], PIB[0], numeroPt[0], calculoPibpercapita(PIB[0], populacao[0])},  
         {populacao[1], area[1], PIB[1], numeroPt[1], calculoPibpercapita(PIB[0], populacao[0])}
     };
-    
-     // População
-    if (cidade[0].populacao > cidade[1].populacao) {
-        printf("População: Carta 1 venceu!\n");
-    } else if (cidade[0].populacao < cidade[1].populacao) {
-        printf("População: Carta 2 venceu!\n");
-    } else {
-        printf("População: Empate!\n");
+
+    //Opções de escolha de atributo
+    printf ("Digite o número do atributo que você quer para determinar o vencedor: \n");
+    printf ("1 - Atributo POPULAÇÃO\n");
+    printf ("2 - Atributo ÁREA\n");
+    printf ("3 - Atributo PIB\n");
+    printf ("4 - Atributo PONTOS TURÍSTICOS\n");
+    printf ("5 - Atributo DENSIDADE POPULACIONAL\n");
+    printf ("6 - Atributo PIB PER CAPITA\n");
+    printf ("7 - Atributo SUPER PODER\n");
+    scanf ("%d", &escolha);  
+
+    switch (escolha){
+        case 1: //População
+            if (cidade[0].populacao > cidade[1].populacao) {
+                printf("População: Carta 1 venceu!\n");
+            } else if (cidade[0].populacao < cidade[1].populacao) {
+                printf("População: Carta 2 venceu!\n");
+            } else {
+                printf("População: Empate!\n");
+            }
+            break;
+        case 2: //Área
+            if (cidade[0].area > cidade[1].area) {
+            printf("Área: Carta 1 venceu!\n");
+            } else if (cidade[0].area < cidade[1].area) {
+            printf("Área: Carta 2 venceu!\n");
+            } else {
+            printf("Área: Empate!\n");
+            }
+            break;
+        case 3: //PIB
+            if (cidade[0].PIB > cidade[1].PIB) {
+                printf("PIB: Carta 1 venceu!\n");
+            } else if (cidade[0].PIB < cidade[1].PIB) {
+                printf("PIB: Carta 2 venceu!\n");
+            } else {
+                printf("PIB: Empate!\n");
+            }
+            break;
+        case 4: //Pontos turísticos
+            if (cidade[0].numeroPt > cidade[1].numeroPt) {
+                printf("Pontos turísticos: Carta 1 venceu!\n");
+            } else if (cidade[0].numeroPt < cidade[1].numeroPt) {
+                printf("Pontos turísticos: Carta 2 venceu!\n");
+            } else {
+                printf("Pontos turísticos: Empate!\n");
+            }
+            break;
+        case 5: //Densidade Populacional
+            if (cidade[0].calculoDensidade > cidade[1].calculoDensidade){
+                printf("Densidade populacional: Carta 2 venceu!\n");
+            } else if (cidade[0].calculoDensidade < cidade[1].calculoDensidade){
+                printf("Densidade populacional: Carta 1 venceu!\n");
+            } else {
+                printf("Densidade populacional: Empate!\n");
+            }
+            break;
+        case 6: //PIB per capita
+            if (cidade[0].calculoPibpercapita > cidade[1].calculoPibpercapita) {
+                printf("PIB per capita: Carta 1 venceu!\n");
+            } else if (cidade[0].calculoPibpercapita < cidade[1].calculoPibpercapita) {
+                printf("PIB per capita: Carta 2 venceu!\n");
+            } else {
+                printf("PIB per capita: Empate!\n");
+            }
+            break;
+        case 7: // Super poder
+            float soma0 = calculoSomaatributos(cidade, 1);
+            float soma1 = calculoSomaatributos(cidade, 1);
+
+            if (soma0 > soma1){
+                printf("Super Trunfo: Carta 1 venceu!\n");
+            } else if (soma0 < soma1) {
+                printf("Super Trunfo: Carta 2 venceu!\n");
+            } else {
+                printf("Super Trunfo: Empate!\n");
+            }
+            break;
     }
-
-    // Área
-    if (cidade[0].area > cidade[1].area) {
-        printf("Área: Carta 1 venceu!\n");
-    } else if (cidade[0].area < cidade[1].area) {
-        printf("Área: Carta 2 venceu!\n");
-    } else {
-        printf("Área: Empate!\n");
-    }
-
-    // PIB
-    if (cidade[0].PIB > cidade[1].PIB) {
-        printf("PIB: Carta 1 venceu!\n");
-    } else if (cidade[0].PIB < cidade[1].PIB) {
-        printf("PIB: Carta 2 venceu!\n");
-    } else {
-        printf("PIB: Empate!\n");
-    }
-
-    // Pontos turísticos
-    if (cidade[0].numeroPt > cidade[1].numeroPt) {
-        printf("Pontos turísticos: Carta 1 venceu!\n");
-    } else if (cidade[0].numeroPt < cidade[1].numeroPt) {
-        printf("Pontos turísticos: Carta 2 venceu!\n");
-    } else {
-        printf("Pontos turísticos: Empate!\n");
-    }
-    //Densidade Populacional
-    if (cidade[0].calculoDensidade > cidade[1].calculoDensidade){
-        printf("Densidade populacional: Carta 2 venceu!\n");
-    } else if (cidade[0].calculoDensidade < cidade[1].calculoDensidade){
-        printf("Densidade populacional: Carta 1 venceu!\n");
-    } else {
-        printf("Densidade populacional: Empate!\n");
-    }
-
-    // PIB per capita
-    if (cidade[0].calculoPibpercapita > cidade[1].calculoPibpercapita) {
-        printf("PIB per capita: Carta 1 venceu!\n");
-    } else if (cidade[0].calculoPibpercapita < cidade[1].calculoPibpercapita) {
-        printf("PIB per capita: Carta 2 venceu!\n");
-    } else {
-        printf("PIB per capita: Empate!\n");
-    }
-
-    // Super poder
-    float soma0 = calculoSomaatributos(cidade, 1);
-    float soma1 = calculoSomaatributos(cidade, 1);
-
-    if (soma0 > soma1)
-        printf("Super Trunfo: Carta 1 venceu!\n");
-    else if (soma0 < soma1)
-        printf("Super Trunfo: Carta 2 venceu!\n");
-    else
-        printf("Super Trunfo: Empate!\n");
-
+   
     // Exibição dos Dados das Cartas:
-    printf ("\nDados cidade 1 ===================================\n");
+    printf ("\n============== Dados cidade 1 ================\n");
     printf ("Código Cidade: %d\n", codigoCidade[0]);
     printf ("Nome da Cidade: %s\n", nomeCidade[0]);
     printf ("Número da população: %d\n", populacao[0]);
     printf ("Área: %f\n", area[0]);
     printf ("PIB da cidade: %f\n", PIB[0]);
     printf ("Número de pontos turísticos: %d\n", numeroPt[0]);
-    printf ("Informações =============================\n");
     printf ("Densidade Populacional: %f\n", calculoDensidade(populacao[0],area[0]));
     printf ("PIB per capita: %d\n", calculoPibpercapita(PIB[0],populacao[0]));
-    printf ("Dados ===================================\n");
 
-    printf ("\nDados cidade 2 ===================================\n");
+    printf ("\n============== Dados cidade 2 ================\n");
     printf ("Código Cidade: %d\n", codigoCidade[1]);
     printf ("Nome da Cidade: %s\n", nomeCidade[1]);
     printf ("Número da população: %d\n", populacao[1]);
     printf ("Área: %f\n", area[1]);
     printf ("PIB da cidade: %f\n", PIB[1]);
     printf ("Número de pontos turísticos: %d\n", numeroPt[1]);
-    printf ("Informações =============================\n");
     printf ("Densidade Populacional: %f\n", calculoDensidade(populacao[1],area[1]));
     printf ("PIB per capita: %d\n", calculoPibpercapita(PIB[1],populacao[1]));
-    printf ("Dados ===================================\n");
 
     return 0;
 }
 
-if (1 > 3 && 2 == 2){
-    return true
-}
+
